581b9051cad2e64096c73233998426dc75ffbe9f
Vanni Durantini <vannivd@yahoo.com>
2019-10-17 08:03:56 +0200

    aggiunto costruttore e getter/setter

--- src/main/java/base/Calcolatrice.java
+++ src/main/java/base/Calcolatrice.java
@@ -16,4 +16,35 @@ public class Calcolatrice {
 	int somma(int x, int y) {
 		return x+y;
 	}
+
+	public Calcolatrice(Date date1, Calendar date2, LocalDate date3) {
+		super();
+		this.date1 = date1;
+		this.date2 = date2;
+		this.date3 = date3;
+	}
+
+	public Date getDate1() {
+		return date1;
+	}
+
+	public void setDate1(Date date1) {
+		this.date1 = date1;
+	}
+
+	public Calendar getDate2() {
+		return date2;
+	}
+
+	public void setDate2(Calendar date2) {
+		this.date2 = date2;
+	}
+
+	public LocalDate getDate3() {
+		return date3;
+	}
+
+	public void setDate3(LocalDate date3) {
+		this.date3 = date3;
+	}
 }
